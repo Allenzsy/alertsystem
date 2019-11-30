238f7b152a7a8fc6371ecfa42012dfab4f5177cb
allenzsy <986847686qq.com>
2019-12-01 00:34:47 +0800

    update reamdme

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,45 +1,78 @@
 
-CREATE TABLE if not exists `t_seckill`(
-    `seckill_id` bigint NOT NULL AUTO_INCREMENT COMMENT '商品库存id',
-    `name` varchar(120) NOT NULL COMMENT '商品名称',
-    `number` int NOT  NULL COMMENT '库存数量',
-    `start_time` timestamp not NULL COMMENT '秒杀开启时间',
-    `end_time` timestamp not NULL COMMENT '秒杀结束时间',
-    `create_time` timestamp  NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    PRIMARY KEY PK_SECKILL_ID(seckill_id),
-    key idx_start_time(start_time),
-    key idx_end_time(end_time),
-    key idx_create_time(create_time)
-) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET = utf8 COMMENT = '秒杀库存表';
-
-
-CREATE TABLE if not exists `t_user`(
-    `id` int NOT NULL AUTO_INCREMENT,
-    `name` varchar (120) NOT NULL,
-    `password` varchar (120) NOT NULL,
-    PRIMARY KEY PK_ID(id)
-) ENGINE=InnoDB DEFAULT CHARSET = utf8;
-
-insert into t_user (name, password) values ('zsy', '123123'),('wk', '123456');
-
+-- t_exmessage 预警信息表
 CREATE TABLE if not  exists `t_exmessage` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `system_id` bigint NOT NULL COMMENT '系统id',
-    `system_name` varchar (30) NOT NULL COMMENT '系统名称',
-    `ex_id` bigint NOT NULL COMMENT '异常类别id',
-    `ex_time` timestamp NOT NULL COMMENT '异常时间',
+    `id` int NOT NULL AUTO_INCREMENT COMMENT '与业务无关自增id',
+    `system_id` bigint NOT NULL COMMENT '发出预警信息的系统id',
+    `token` char(5) NOT NULL COMMENT '用于验证系统',
     `ex_description` varchar (120) NOT NULL COMMENT '异常描述',
-    PRIMARY KEY (id),
-    key idx_system_ex(system_id, ex_id),
-    key idx_ex_time(ex_time)
+    `ex_occurtime` timestamp NOT NULL COMMENT '异常信息在系统发生的时间',
+    `ex_createtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '预警信息入库时间',
+    PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET = utf8;
 
-CREATE TABLE if not  exists `t_notice` (
+
+-- t_system 系统表
+CREATE TABLE if not  exists `t_system` (
+    `id` bigint NOT NULL AUTO_INCREMENT COMMENT '系统id',
+    `system_name` varchar (16) NOT NULL COMMENT '系统名称',
+    `system_description` varchar (50) NOT NULL COMMENT '系统描述',
+    PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET = utf8 COMMENT = '系统表';
+
+
+-- t_system_user 系统-负责人映射表
+CREATE TABLE if not  exists `t_system_user` (
     `system_id` bigint NOT NULL COMMENT '系统id',
-    `admin_name` varchar (16) NOT NULL,
-    `admin_mail` varchar (50) NOT NULL,
-    PRIMARY KEY (system_id)
-) ENGINE=InnoDB DEFAULT CHARSET = utf8;
+    `user_id` int (16) NOT NULL COMMENT '负责人id',
+    PRIMARY KEY (system_id, user_id)
+) ENGINE=InnoDB DEFAULT CHARSET = utf8 COMMENT = '系统-负责人映射表';
+
+
+-- t_user 负责人表
+CREATE TABLE if not  exists `t_user` (
+    `id` int NOT NULL AUTO_INCREMENT COMMENT '负责人id',
+    `user_name` varchar (16) NOT NULL COMMENT '负责人姓名',
+    `mail` varchar (20) COMMENT '邮箱',
+    `qq` varchar (20) COMMENT 'qq',
+    `phone` varchar (20) COMMENT '手机号',
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET = utf8 COMMENT = '负责人表';
+
+
+-- t_rule 发送通知规则表
+CREATE TABLE if not  exists `t_rule` (
+    `id` int NOT NULL AUTO_INCREMENT COMMENT '发送通知的规则 id',
+    `frequency` int NOT NULL COMMENT '发送频率',
+    `rank_id` bigint NOT NULL COMMENT '预警级别 id',
+    `sender_id` int NOT NULL COMMENT '发送通知器 id',
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET = utf8 COMMENT = '发送通知规则表';
+
+
+-- t_rule_user 发送通知规则-负责人映射表
+CREATE TABLE if not  exists `t_rule_user` (
+    `rule_id` int NOT NULL COMMENT '发送通知的规则 id',
+    `user_id` int NOT NULL COMMENT '负责人id',
+    PRIMARY KEY (user_id, rule_id)
+) ENGINE=InnoDB DEFAULT CHARSET = utf8 COMMENT = '发送通知规则-负责人映射表';
+
+
+-- t_sender 发送器配置表
+CREATE TABLE if not  exists `t_sender` (
+    `id` int NOT NULL AUTO_INCREMENT COMMENT '发送通知的规则 id',
+    `config` varchar(100) NOT NULL COMMENT '发送器配置，Json字符串形式',
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET = utf8 COMMENT = '发送器配置表';
+
+
+-- t_rank 预警级别表
+CREATE TABLE if not  exists `t_rank` (
+    `id` bigint NOT NULL COMMENT '预警级别 id',
+    `rank` varchar(10) NOT NULL COMMENT '预警级别 (e.g. error/warning/info)',
+    PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=200 DEFAULT CHARSET = utf8 COMMENT = '预警级别表';
+
+
 
 insert into t_notice values (10001, '张经理', '986847686@qq.com');
 
